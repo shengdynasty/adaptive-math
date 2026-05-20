1d66b5ff388a9e6acdc88808cfdbfb14fbb3585c
shengdynasty <shengyan555@gmail.com>
2026-05-19 20:45:56 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/adaptive-math/sw.js', { scope: '/adaptive-math/' })})}
